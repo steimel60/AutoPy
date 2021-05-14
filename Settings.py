251a31c030c7dda6557d4b147aee432c19709d72
landonleighredline <83823882+landonleighredline@users.noreply.github.com>
2021-05-14 09:27:49 -0400

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -2,15 +2,13 @@ import os
 import shutil
 import glob
 
-
-
 ####### PATHS TO FILES #######
 new_job_template = r'C:\Users\DylanSteimel\Desktop\TestTemplate'
 new_job_folder = r'C:\Users\DylanSteimel\Desktop'
 
-
 ########## APP PATHS ###############
 scene_path = 'C:\Program Files\FARO\SCENE\SCENE.exe'
+pix_mapper_path = 'C:\Program Files\Pix4Dmapper\pix4dmapper.exe'
 
 ####### SERVER PATHS #############
 CLT = r'C:\Users\DylanSteimel\Documents\Fake_Server\CLT'
@@ -24,7 +22,6 @@ scan_folder = r'\Scans'
 processed_folder = r'\Processed'
 scene_folder = r'\Scene'
 pix4d_folder = r'\Pix4D'
-
 
 ############# Text File Path #####################
 text_path = 'C:\\Users\\DylanSteimel\\Documents\\Fake_Server\\'
